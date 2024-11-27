97c36ed50590493ee5e8b7450888acc92782aff8
Anna Sarambelieva <ansa@milestone.dk>
2024-11-27 17:35:01 +0200

    Initial commit

---
+++ cscb869-graduation/src/main/java/com/cscb869_graduation/Cscb869GraduationApplication.java
@@ -0,0 +1,13 @@
+package com.cscb869_graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cscb869GraduationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cscb869GraduationApplication.class, args);
+	}
+
+}
---
+++ cscb869-graduation/src/test/java/com/cscb869_graduation/Cscb869GraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cscb869_graduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cscb869GraduationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
